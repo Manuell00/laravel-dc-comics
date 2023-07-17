77b0a97272edc5d2c60cd3487fd9627ab4e59f5a
Manuell00 <manuell.caselli@gmail.com>
2023-07-17 17:09:21 +0200

    Creo il controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// Importo il model
+use App\Models\Comic;
+
+class MainController extends Controller
+{
+    //Index, ritorno tutti i dati della tabella e li metto in comics.index
+    public function index()
+    {
+        $comics = Comic::all();
+
+        return view("comic.index", compact("comics"));
+    }
+}
