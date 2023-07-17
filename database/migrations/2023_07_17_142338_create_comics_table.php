6a5a949bf09cd9b0142bc7f3aa447eb41f0baf22
Manuell00 <manuell.caselli@gmail.com>
2023-07-17 16:29:22 +0200

    Creazione tabella

---
+++ database/migrations/2023_07_17_142338_create_comics_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            // Inserisco i campi della table 
+            $table->string('title', 64);
+            $table->text('description');
+            $table->text('thumb', 64);
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
