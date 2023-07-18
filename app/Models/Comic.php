fe3ef4340416447d3a2b027ff479bdce33a56666
Manuell00 <manuell.caselli@gmail.com>
2023-07-18 10:07:09 +0200

    Importo dati con il seed nella tab

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    // Inserisco i dati da proteggere
+    protected $fillable = [
+        'title',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'sale_date',
+        'type'
+    ];
 }
